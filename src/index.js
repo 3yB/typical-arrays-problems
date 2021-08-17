788e64b11b76703642cf525cbfa76b263fd0baab
3yB <kempoman1991@gmail.com>
2021-08-17 13:17:06 +0300

    feat: add index.js file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
 exports.min = function min (array) {
+    
+    if (typeof array != `undefined` && array.length > 0) {
+        let minNumb = array[0];
+
+        for (let i = 1; i < array.length; i++) {
+            array[i] < minNumb ? minNumb = array[i] : false;
+        }
+        return minNumb;
+    } 
+
   return 0;
 }
 
 exports.max = function max (array) {
+
+  if (typeof array != `undefined` && array.length > 0) {
+    let maxNumb = array[0];
+
+    for (let i = 1; i < array.length; i++) {
+        array[i] > maxNumb ? maxNumb = array[i] : false;
+    }
+    return maxNumb;
+} 
+
   return 0;
 }
 
 exports.avg = function avg (array) {
+
+  if (typeof array != `undefined` && array.length > 0) {
+    return array.reduce( (a, b) => a + b) / array.length;
+} 
+
   return 0;
 }
